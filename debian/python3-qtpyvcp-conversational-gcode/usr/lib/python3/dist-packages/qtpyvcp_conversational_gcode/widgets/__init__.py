0ee96b66719d4e1b571c8ecf46ea4f7136ab527f
lcvette <lcvette1@gmail.com>
2023-02-25 18:35:08 -0500

    build debian package install, used all deps

---
+++ debian/python3-qtpyvcp-conversational-gcode/usr/lib/python3/dist-packages/qtpyvcp_conversational_gcode/widgets/__init__.py
@@ -0,0 +1,33 @@
+from qtpyvcp.widgets.qtdesigner import _DesignerPlugin
+
+from qtpyvcp_conversational_gcode.widgets.facing import *
+from qtpyvcp_conversational_gcode.widgets.facing import FacingWidget
+from qtpyvcp_conversational_gcode.widgets.xy_coord import XYCoordWidget
+from qtpyvcp_conversational_gcode.widgets.hole_circle import HoleCircleWidget
+from qtpyvcp_conversational_gcode.widgets.int_line_edit import IntLineEdit
+from qtpyvcp_conversational_gcode.widgets.float_line_edit import FloatLineEdit
+
+
+class FloatLineEditPlugin(_DesignerPlugin):
+    def pluginClass(self):
+        return FloatLineEdit
+
+
+class IntLineEditPlugin(_DesignerPlugin):
+    def pluginClass(self):
+        return IntLineEdit
+
+
+class HoleCircleWidgetPlugin(_DesignerPlugin):
+    def pluginClass(self):
+        return HoleCircleWidget
+
+
+class XYCoordWidgetPlugin(_DesignerPlugin):
+    def pluginClass(self):
+        return XYCoordWidget
+
+
+class FacingWidgetPlugin(_DesignerPlugin):
+    def pluginClass(self):
+        return FacingWidget
